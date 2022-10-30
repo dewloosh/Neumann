38f49ad4888955eb23c894a3a9500ac49f62300c
Bence Balogh <benceeok@gmail.com>
2022-10-30 21:22:29 +0100

    Update test_interval.py

--- tests/test_interval.py
+++ tests/test_interval.py
@@ -6,34 +6,34 @@ from numba import jit
 from neumann.interval import Interval
 
 
-@jit(nopython=True)
-def inside_interval(interval, x):
-    return interval.lo <= x < interval.hi
-
-
-@jit(nopython=True)
-def interval_width(interval):
-    return interval.width
-
-
-@jit(nopython=True)
-def interval_data(interval):
-    return interval.data
-
-
-@jit(nopython=True)
-def interval_getitem(interval, i):
-    return interval[i]
-
-
-@jit(nopython=True)
-def new_interval(lo, hi, data):
-    return Interval(lo, hi, data)
-
-
 class TestInterval(unittest.TestCase):
 
     def test_interval(self, lo=1.0, hi=3.0):
+        
+        @jit(nopython=True)
+        def inside_interval(interval, x):
+            return interval.lo <= x < interval.hi
+
+
+        @jit(nopython=True)
+        def interval_width(interval):
+            return interval.width
+
+
+        @jit(nopython=True)
+        def interval_data(interval):
+            return interval.data
+
+
+        @jit(nopython=True)
+        def interval_getitem(interval, i):
+            return interval[i]
+
+
+        @jit(nopython=True)
+        def new_interval(lo, hi, data):
+            return Interval(lo, hi, data)
+        
         data = np.array([1.1, 3.1, 2.1])
         new_interval(lo, hi, data)._arr
         interval_data(new_interval(lo, hi, data))
@@ -50,4 +50,4 @@ if __name__ == "__main__":
     interval_data(new_interval(lo, hi, data))
     interval_getitem(new_interval(lo, hi, data), 0)"""
 
-    #unittest.main()
+    unittest.main()
