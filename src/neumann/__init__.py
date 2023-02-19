c7881e467e25e7854160c30abcd28521002d5332
Bence Balogh <benceeok@gmail.com>
2023-02-19 15:16:09 +0100

    new version

--- src/neumann/__init__.py
+++ src/neumann/__init__.py
@@ -2,6 +2,6 @@ from .hist import *
 from .utils import *
 from .decorate import *
 
-__version__ = "0.0.10"
+__version__ = "1.0.0"
 
 __description__ = "A Python Library for Applied Mathematics in Physical Sciences."
